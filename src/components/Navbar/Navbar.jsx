ff463d908d9ef858b30a8241b8a3b337e83f32d8
Buntu William <williambuntu1@gmail.com>
2023-08-13 20:58:00 +0200

    3nd commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import './Navbar.scss'
+import { images } from '../../constants'
+
+
 const Navbar = () => {
   return (
       <nav>
 
 
                   <div>
-                       <img  src='' alt='logo'/>
+                       <img  src={images.logo} alt='logo'/>
+
+                        <ul>
+                        {['home', 'about', 'contact', 'work', 'skills, '].map( (item) => (
+                           <li key={`link-${item}`}>
+                            <div>
+
+                                <a href={`#${item}`}> {item} </a>
+                            </div>
+
+                           </li>
+                        )) }
+                        </ul>
                 </div>
 
 
